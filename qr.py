50a7dce0943e55a18d64a2511523f3c5ce906a14
virejdasani <dasanivirej@gmail.com>
2020-09-09 10:41:19 +0530

    Initial Commit

---
+++ qr.py
@@ -0,0 +1,8 @@
+import qrcode
+
+# This code will generate a .png file with the QR Code of the website entered
+
+site = input("Enter web address for QR code generation\n")
+
+image = qrcode.make(site)
+image.save(f'qr{site}.png', 'PNG')
